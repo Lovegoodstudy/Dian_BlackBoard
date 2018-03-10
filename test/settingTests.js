b3cc1d1a921face184c2bb3d859f1bf86d277c0f
lovegood <287310886@qq.com>
2018-03-10 15:56:25 +0800

    test Setting APIs add 2 test

--- test/settingTests.js
+++ test/settingTests.js
@@ -9,8 +9,8 @@ describe('Board Setting API', function() {
         done();
     });
     
-    describe('Setting', function() {
-        it('Should setting success', function(done){
+    describe('Setting APIs', function() {
+        it('Should post setting success', function(done){
             let settingData = {
                 "pictureInfomations": [
                     {
@@ -35,6 +35,45 @@ describe('Board Setting API', function() {
             request(url)
                 .post('/setting')
                 .send(settingData)
+                .expect(200) //Status code
+                .end(function(err,res) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("success res==>",res.body);
+                    // Should.js fluent syntax applied
+                    done();
+                });
+        });
+        it('Should post setting fail', function(done){
+            let settingData = {
+                "pictureInfomations": [
+                    {
+                        "filepath": "./images/1.jpg"
+                    }
+                ],
+                "appointmentInfomations": {
+                    "displayTime": "10",
+                    "refreshTime": "120"
+                }
+            };
+            
+            request(url)
+                .post('/setting')
+                .send(settingData)
+                .expect(400) //Status code
+                .end(function(err,res) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("success res==>",res.body);
+                    // Should.js fluent syntax applied
+                    done();
+                });
+        });
+        it('Should get setting success', function(done){
+            request(url)
+                .get('/setting')
                 .expect(200) //Status code
                 .end(function(err,res) {
                     if (err) {
