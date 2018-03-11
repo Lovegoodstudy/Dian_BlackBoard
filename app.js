aeb96913da0c8a8959e0c1d0c204e654c755a296
RuisongZhou <811437508@qq.com>
2018-03-11 18:04:33 +0800

    add upload API

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer  = require('multer');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,18 +13,20 @@ var setting = require('./routes/setting');
 
 
 var app = express();
-
+//app.use(bodyParser({ keepExtensions: true, uploadDir:  path.join(__dirname, 'images') }));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({ dest: '/tmp/'}).array('image'));
 
 app.use('/', index);
 app.use('/users', users);
--- routes/image.js
+++ routes/image.js
@@ -4,6 +4,8 @@ let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 let fs = require('fs'); 
 
+var multer  = require('multer');
+
 router.get('/', urlencodedParser, function (req, res, next) {
     let getpath=('./public/images');
     fs.readdir(getpath, function(err, files) {  
@@ -60,6 +62,36 @@ router.delete('/', urlencodedParser, function (req, res, next) {
 
     }
 });
+
+
+
+
+router.post('/', urlencodedParser, function (req, res, next) {
+
+    console.log(req.files[0]);  // 上传的文件信息
+    
+    var des_file = "./public/images" + "/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+        if( err ){
+            console.log( err );
+        }else{
+                response = {
+                    message:'File uploaded successfully', 
+                    filename:req.files[0].originalname
+            };
+        }
+        console.log( response );
+
+
+        res.end( JSON.stringify( response ) );
+        });
+    });
+   
+});
+
+
+
 
 function _validateDeleteData(deleteData){
     let deletePath =("./public/images/");
